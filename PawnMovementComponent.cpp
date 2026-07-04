e3a54d29c30e9ced77a82c73a822f7d49a789c41
Wr3tchedTorch <ericmoura511@gmail.com>
2026-07-04 10:24:53 -0300

    fixed pawns being able to go backwards

--- PawnMovementComponent.cpp
+++ PawnMovementComponent.cpp
@@ -5,15 +5,21 @@
 #include <SFML/System/Vector2.hpp>
 
 #include "StraightMovementComponent.h"
+#include "Team.h"
 
 bool chess::PawnMovementComponent::IsPositionReachable(const sf::Vector2u& current_pos, const sf::Vector2u& target_pos, bool occupied_by_enemy) const noexcept
-{
+{   
+    auto distance_y = static_cast<int>(target_pos.y - current_pos.y);
+    bool going_forwards = team_ == chess::Team::White ? distance_y < 0 : distance_y > 0;
+    if (!going_forwards)
+    {
+        return false;
+    }
     if (!occupied_by_enemy)
     {
         return StraightMovementComponent::IsPositionReachable(current_pos, target_pos, occupied_by_enemy);
     }
     auto distance_x = static_cast<int>(target_pos.x - current_pos.x);
-    auto distance_y = static_cast<int>(target_pos.y - current_pos.y);
 
     return abs(distance_x) == abs(distance_y);
 }
